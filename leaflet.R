60aae68fb1211e9057676fa887e729742999b8e9
Antony Samarawickrema <antonysama@gmail.com>
2016-02-15 20:51:43 -0700

    Update leaflet.R

--- leaflet.R
+++ leaflet.R
@@ -1,23 +1,26 @@
 install.packages("leaflet")
 library(leaflet)
 locations<-read.csv("locations.csv", stringsAsFactors=FALSE)
-locations<-locations[c(1:10,12)]
+locations<-locations[c(1:10,12)] #deleted no. of building permits
+
+#renames columns
 names(locations)<-c("region","name", "lat", "long", 
                     "mdn_age", "below_15", 
                     "post_sec", "income", "growth", "buildings",
-                    "projects")
-
-na_post_sec<-locations[which(is.na(locations$post_sec)), 1:2]
+                    "projects") 
+#selects locatons with na's
+na_post_sec<-locations[which(is.na(locations$post_sec)), 1:2] 
 na_income<-locations[which(is.na(locations$income)), 1:2]
 
-
+#orders indicators of interest
 below_15<-tail(locations[ order(locations[,"below_15"]),], n=7)
 post_sec<-head(locations[ order(locations[,"post_sec"], 
                                 locations[,"name"]),], n=7)
 mdn_age <-subset(locations,mdn_age>35 & mdn_age<45)
 income<-head(locations[ order(locations[,"income"]),], n=7)
 growth<-head(locations[ order(locations[,"growth"]),], n=7)
-                              
+
+#maps indicators                              
 leaflet(data = locations[c(1:30),]) %>% addTiles() %>%
   addMarkers(~long, ~lat, popup = ~as.character(name))
 
@@ -36,10 +39,12 @@ leaflet(data = income[c(1:7),]) %>% addTiles() %>%
 leaflet(data = growth[c(1:7),]) %>% addTiles() %>%
   addMarkers(~long, ~lat, popup = ~as.character(name))
 
+#subseets regions
 North<-subset(locations, region=="North")
 CS<-subset(locations, region=="CS")
 Edmonton<-subset(locations, region=="Edmonton")
 
+#box plots regional income& growth
 boxplot(income~region, data = locations, 
         main="Income data (regional dashboard~2013)",
         xlab="Location", ylab="Median family income($)")
@@ -48,10 +53,12 @@ boxplot(growth~region, data = locations,
         main="Income growth data (regional dashboard~2013)",
         xlab="Location", ylab="Median family rowth(#.##)")
 
+#histogram of regional income
 hist(North$income, xlim=c(60000,200000))
 hist(Edmonton$income, xlim=c(60000,200000))
 hist(CS$income, xlim=c(60000,200000))
 
+#box plots regional projects & building permits
 boxplot(projects~region, data = locations, 
         main="Value of major projects (IMAP~2013-17)",
         xlab="Location", ylab="Value($)")
