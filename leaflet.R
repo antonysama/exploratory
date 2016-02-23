78aa3d0f78b75a13edd44592b64d578643754b39
Antony Samarawickrema <antonysama@gmail.com>
2016-02-22 20:07:50 -0700

    Update leaflet.R

--- leaflet.R
+++ leaflet.R
@@ -2,15 +2,16 @@
 getwd()
 list.files()
 locations<-read.csv("locations_1.1.csv")
-locations<-locations[c(1:10,12,13,15)]  #Take what we need
+locations<-locations[c(1:8,10,12,13,14,15)]  #Take what we need
 str(locations)  
 head(locations, n=4)
 
 #if needed...renames columns
 names(locations)<-c("region","name", "lat", "long", 
                     "age", "children", 
-                    "education", "income", "growth", "buildings",
-                    "projects", "tax", "small_biz")
+                    "education", "income",  "buildings",
+                    "projects", "tax", "ratesprd", "small_biz")
+as.numeric("children", "education", "tax",  "small_biz")
 head(locations, n=1)
 
 #if needed ...creates an index 
@@ -29,8 +30,8 @@ education_head<-head(locations[ order(locations[,"education"],
 new_locations<-na.omit(locations[c(1:7)])
 education_tail<-tail(new_locations[ order(new_locations[,"education"], 
                                      new_locations[,"name"]),], n=7)
-age <-subset(locations,age>35 & age<45)
-rvs_age <-subset(locations,age<35 | age>45)
+SnstvAge <-subset(locations,age>35 & age<55)
+Adptv_age <-subset(locations,age<35 | age>55)
 
 income_head<-head(locations[ order(locations[,"income"]),], n=7)
 income_tail<-tail(locations[ order(locations[,"income"]),], n=7) 
@@ -87,19 +88,46 @@ leaflet(data = small_biz_head[c(1:7),]) %>% addTiles() %>%
 leaflet(data = small_biz_tail[c(1:7),]) %>% addTiles() %>%
   addMarkers(~long, ~lat, popup = ~as.character(name))
 #subseets regions
-North<-subset(locations, region=="North")
-CS<-subset(locations, region=="CS")
-Edmonton<-subset(locations, region=="Edmonton")
+BR<-subset(locations, region=="Battle River")
+Gns<-subset(locations, region=="Genessee")
+Sh<-subset(locations, region=="Sheerness")
+Mlnr<-subset(locations, region=="Milner")
+
 
 #box plots regional income& growth
 boxplot(income~region, data = locations, 
-        main="Income data (regional dashboard~2013)",
-        xlab="Location", ylab="Median family income($)")
+        main="Median Family Income (AB Regional dashboard~2013)",
+        xlab="Location", ylab="income($)")
 
-boxplot(growth~region, data = locations, 
-        main="Income growth data (regional dashboard~2013)",
-        xlab="Location", ylab="Median family rowth(#.##)")
+boxplot(children~region, data = locations, 
+        main="Proportion  of Children  (Statistics Canada~2011)",
+        xlab="Location", ylab="Proportion in popln.")
+boxplot(children~region, data = locations, 
+        main="Proportion  of Children  (Statistics Canada~2011)",
+        xlab="Location", ylab="Proportion in popln.")
 
+boxplot(age~region, data = locations, 
+        main="Median Age of Population(Statisitcs Canada~2011)",
+        xlab="Location", ylab="Median Age(yrs)")
+
+boxplot(education~region, data = locations, 
+        main="Proportion w/ PostSec. Education 
+        (AB Regional dashboard~2011,2014)",
+        xlab="Location", ylab="Proportion in popln.")
+
+boxplot(education~region, data = locations, 
+        main="Proportion w/ PostSec. Education 
+        (AB Regional dashboard~2011,2014)",
+        xlab="Location", ylab="Proportion in popln.")
+
+boxplot(ratesprd~region, data = locations, 
+        main="Rate Spread of Property Tax 
+        (Municipal Affairs,2015)",
+        xlab="Location", ylab="Non Res minus Residential Rate (%)")
+boxplot(small_biz~region, data = locations, 
+        main="Proportion of  Small Businesses(employees<49)
+        (Statisitcs  Canada,2014)",
+        xlab="Location", ylab="Proportion of businesses (%)")
 #histogram of regional income
 hist(North$income, xlim=c(60000,200000))
 hist(Edmonton$income, xlim=c(60000,200000))
@@ -114,6 +142,7 @@ boxplot(buildings~region, data = locations,
         main="Value of building permits (regional dashboard~2014)",
         xlab="Location", ylab="Value of building permits($)")
 
+#Batch geocoder: http://www.mapdevelopers.com/batch_geocode_tool.php
 write.csv(children_head, file = "children_head.csv")
 write.csv(children_tail, file = "children_tail.csv")
 write.csv(education_head, file = "education_head.csv")
@@ -126,5 +155,3 @@ write.csv(tax_head, file = "tax_head.csv")
 write.csv(tax_tail, file = "tax_tail.csv")
 write.csv(small_biz_head, file = "small_biz_head.csv")
 write.csv(smal_biz_tail, file = "small_biz_tail.csv")
-
-#Batch geocoder: http://www.mapdevelopers.com/batch_geocode_tool.php
