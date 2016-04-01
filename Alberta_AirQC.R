b79aeb194068095c298a6434bec6b2761de29f7e
Antony Samarawickrema <antonysama@gmail.com>
2016-04-01 17:06:41 -0600

    Added files via upload

--- Alberta_AirQC.R
+++ Alberta_AirQC.R
@@ -2,19 +2,19 @@
 df<-read.csv("Air.csv")
 df<-df[,c(2,3,5,6)]
 names(df)[1:3]<-c("Name","Year","Risk")
+nozerodf <- df[cumsum(df[,4]) > 0]
 
 #dplyr to get fold (avg) up risk levels 1-10
 library("dplyr") 
 e<-df%>% 
   group_by(Name,Year,Risk)%>% 
-  summarise_each(funs(sum, mean))
-#further fold (avg) up the years
-f<-e[,c(1:4)]%>% 
-  group_by(Name,Risk)%>% 
-  summarise_each(funs(mean))
+  summarise_each(funs(sum))
+
+#Replace 0s with NA 
+e[e==0] <- NA
 
 #Reshape to take the risk levels to headings 
-library("reshape2")
-fmelt<-melt(f[,c(1,2,4)], id=c("Name","Risk"), measure.vars=c("sum"))
-fcast<-dcast(fmelt, Name~ Risk)
+library("reshape")
+fmelt<-melt(e[,c(1,3,4)], id=c("Name","Risk"), measure.vars=c("Value"))
+fcast<-dcast(fmelt, Name~ Risk, mean, na.rm=T)
 write.csv(fcast,file = "fcastAir.csv")
