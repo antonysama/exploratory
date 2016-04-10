f840b47945b4385760889cae32b5106f92895a2e
Antony Samarawickrema <antonysama@gmail.com>
2016-04-10 16:42:47 -0600

    Added files via upload

--- ReadingJson.R
+++ ReadingJson.R
@@ -12,9 +12,9 @@ library("tm")
 json = '[{"name":"John","age":20, "sex": "M"}, {"name":"Martin","age":30, "sex":"t"},{"name":"Mrin","age":50, "sex":"F"}]]'
 Data2 = unlist(fromJSON(json))
 # dwnld json files
-url2<-paste0("http://jsonstudio.com/wp-content/uploads/2014/02/companies.zip")
-download.file(url2,dest="companies.zip") 
-unzip("companies.zip")
+url2<-paste0("http://jsonstudio.com/wp-content/uploads/2014/02/world_bank.zip")
+download.file(url2,dest="world_bank.zip") 
+unzip("world_bank.zip")
 # Create a file named "texts" where you'll keep your data.
 # Save the file to a particular place
 #save the folder to your C: drive and use the following code chunk:
@@ -30,15 +30,17 @@ docs <- tm_map(docs, removeWords, stopwords("english"))
 docs <- tm_map(docs, stripWhitespace)
 docs <- tm_map(docs, PlainTextDocument) 
 dtm <- DocumentTermMatrix(docs)   
-dtm   
-inspect(dtm[1, 1:10])
-dim(dtm)
-#Iinvs matrix
-tdm <- TermDocumentMatrix(docs)  
 #  Start by removing sparse terms:   
-dtms <- removeSparseTerms(dtm, 0.1) # This makes a matrix that is 10% empty space, maximum. 
-#word freq
-freq[head(ord)]
-freq[tail(ord)]
-freq <- colSums(as.matrix(dtms))   
-freq 
+dtmss <- removeSparseTerms(dtm, 0.15) # This makes a matrix that is 15% empty space, maximum. 
+#hclust
+library("cluster")   
+library("rafalib")
+d <- dist(t(dtmss))   
+reduced <- dtmss[sample(nrow(dtmss),80),]
+hc<-hclust(dist(t(dtmss)))  
+plot(hc, hang=-1)
+#knn
+library("fpc")   
+d <- dist(t(dtmss), method="euclidian")   
+kfit <- kmeans(dist(t(dtmss)), centers= 3)   
+clusplot(as.matrix(d), kfit$cluster, color=T, shade=T, labels=2, lines=0)  
