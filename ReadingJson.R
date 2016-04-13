321467c341bc6ffe1077c0828697b81aa43019bb
Antony Samarawickrema <antonysama@gmail.com>
2016-04-12 22:18:58 -0600

    Added files via upload

--- ReadingJson.R
+++ ReadingJson.R
@@ -19,8 +19,8 @@ json3 = '[{"name":"Jean","age":23, "sex": "M"}, {"name":"Min","age":40, "sex":"t
 cat(json, file="C:/texts/cat3.json")
 
 # or dwnld json files
-url<-paste0("http://json.org/example.html")
-download.file(url,dest="xampl.json") 
+url<-paste0("http://www.pembina.org/blog/fact-checking-the-coal-industrys-information-meetings-part-2")
+download.file(url,dest="xampl3.html") 
 unzip("enron.zip")
 # Create a file named "texts" where you'll keep your data.
 # Save the file to a particular place
@@ -37,19 +37,38 @@ docs <- tm_map(docs, tolower)
 docs <- tm_map(docs, removeWords, stopwords("english"))
 docs <- tm_map(docs, stripWhitespace)
 docs <- tm_map(docs, PlainTextDocument) 
-
+#Removing common word endings (e.g., "ing", "es", "s")
+library("SnowballC")   
+docs <- tm_map(docs, stemDocument)  
 #create dtm and tdm
 dtm <- DocumentTermMatrix(docs) 
 tdm <- TermDocumentMatrix(docs)  
+# removing sparse terms:  
+tdmss <- removeSparseTerms(tdm, 0.20) # This makes a matrix that is 15% empty space, maximum. 
+dtmss <- removeSparseTerms(dtm, 0.20)
 
-#removing sparse terms:  
-tdmss <- removeSparseTerms(tdm, 0.15) # This makes a matrix that is 15% empty space, maximum. 
-dtmss <- removeSparseTerms(dtm, 0.15)
+#word freq
+#org by freq
+freq <- colSums(as.matrix(dtm))  
+ord <- order(freq) 
+freq[tail(ord, n=10)] 
+
+#Term Correlations
+findAssocs(dtm, c("coal" , "pollut"), corlimit=0.98)
+
+#wordcloud
+library("wordcloud")
+#100 most frequently used words.
+set.seed(142)   
+wordcloud(names(freq), freq, max.words=20, colors=brewer.pal(25, "Dark2"))
+
+#clustering by term similarity
+inspect(dtm)
 
 #hclust
 #reduced <- tdmss[sample(1:nrow(tdmss),nrow(tdmss)/5),]
-d <- dist(t(tdmss), method = "euclidian")
-hc<-hclust(d, method="single")
+d <- dist(t(dtm), method = "euclidian")
+hc<-hclust(d, method="complete")
 plot(hc, hang = -1)
 rect.hclust(hc,k=5)
 library("rafalib", lib.loc="C:/R-3.2.4/library")
@@ -59,15 +78,15 @@ dev.off()
 #knn
 library("cluster")
 d <- dist(t(dtmss), method = "euclidian")   
-kfit <- kmeans(dist(dtmss), centers=5)
+kfit <- kmeans(dist(dtmss), centers=2)
 clusplot(as.matrix(d), kfit$cluster, color=T, shade=T, labels=2, lines=0) 
-clara(as.matrix(d), 4, samples = 50)
-clarax
-
 dev.off()
+#try clara for clustering
+clara(as.matrix(d), 10, samples = 100)
+
 
 # Levenshtein Distance
-d  <- adist(dtmss)
+d  <- adist(tdmss)
 rownames(d) <- dtmss
 hc <- hclust(as.dist(d), method="single")
 plot(hc)
